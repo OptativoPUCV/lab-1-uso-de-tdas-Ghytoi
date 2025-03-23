3c073bd24d7c0dd6b21ee95ea8a67579ac485aed
Your Name <you@example.com>
2025-03-23 19:24:31 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -150,8 +150,8 @@ int parentesisBalanceados(char *cadena)
    }
    while (top(pila1) != NULL)
    {
-      char *lastValor1 = pop(pila1);
-      char *lastValor2 = pop(pila2);
+      char lastValor1 = pop(pila1);
+      char lastValor2 = pop(pila2);
 
       if (lastValor1 == '40' && lastValor2 == '41') balanceado++;
       else if (lastValor1 == '91' && lastValor2 == '93') balanceado++;
