45661f5d1b56628b9a74093dffc438683ca9bab3
Your Name <you@example.com>
2025-03-23 16:20:59 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -45,9 +45,7 @@ List* crea_lista()
 {
    List* L = create_list();
 
-   int control; 
-
-   L = (List *) malloc(10 * sizeof(List*));
+   L = (List *) malloc(10 * sizeof(List));
    
    return L;
 }
